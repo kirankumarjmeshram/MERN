c8ca2e312a37ca521649599f3db2b828693528f3
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-04-17 16:09:23 +0530

    minor changes

--- CRUD/src/models/product.model.js
+++ CRUD/src/models/product.model.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const productSchema = mongoose.Schema({
     img1:{type:String,required:true},
-    img2:{type:String,required:true},
+    img2:{type:String,required:false},
     name:{type:String,required:true},
     price1:{type:Number,required:true},
-    price2:{type:Number,required:true},
+    price2:{type:Number,required:false},
     ram:{type:Number,required:true},
     battery:{type:Number,required:true},
 },{
