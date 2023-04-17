491e32e52636504f6e361a9d9a21093a4b96d0f1
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-04-17 14:17:18 +0530

    user and product models are added

---
+++ CRUD/src/models/product.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+    img1:{type:String,required:true},
+    img2:{type:String,required:true},
+    name:{type:String,required:true},
+    price1:{type:Number,required:true},
+    price2:{type:Number,required:true},
+    ram:{type:Number,required:true},
+    battery:{type:Number,required:true},
+},{
+    versionKey:false,
+    timestamps:true,
+})
+
+module.exports = mongoose.model("product",productSchema);
---
+++ CRUD/src/models/user.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    first_name:{type: String,required:true},
+    first_name:{type: String,required:true},
+    email:{type: String,required:true},
+    password:{type: String,required:true},
+    address:{type: String,required:false},
+    pin_code:{type: Number,required:false},
+    cart_product:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"product",
+        required:false,
+    }]
+},{
+    versionKey:false,
+    timestamps:true,
+});
+
+module.exports = mongoose.model("user",userSchema);
