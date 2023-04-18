703add6740fb0fe4278ff5fd35992430051a92bb
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-04-18 17:44:20 +0530

    modified file

--- CRUD/src/configs/db.js
+++ CRUD/src/configs/db.js
@@ -1,18 +1,18 @@
 
-const dotenv = require("dotenv");
-dotenv.config();
-const PASS = process.env.PASS;
+// const dotenv = require("dotenv");
+// dotenv.config();
+// const PASS = process.env.PASS;
 const mongoose = require("mongoose");
 module.exports =()=>{
-    //mongoose.connect('mongodb://127.0.0.1:27017/crudAppData');
+    mongoose.connect('mongodb://127.0.0.1:27017/crudAppData');
 
-    try{
-        mongoose.connect(`mongodb+srv://kirankumar21895:${PASS}@projects.hr7o8em.mongodb.net/test?retryWrites=true`,{
-        useNewUrlParser:true,
-        useUnifiedTopology:true
-    })
-    }catch(err){
-        res.status(500).json({message:err.message,status:"Failed"})
-    }
+    // try{
+    //     mongoose.connect(`mongodb+srv://kirankumar21895:${PASS}@projects.hr7o8em.mongodb.net/test?retryWrites=true`,{
+    //     useNewUrlParser:true,
+    //     useUnifiedTopology:true
+    // })
+    // }catch(err){
+    //     res.status(500).json({message:err.message,status:"Failed"})
+    // }
  }
     
