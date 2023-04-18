06cce5b4cc5f3fc2a916ffc47b984844ad89aa3c
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-04-18 15:11:10 +0530

    connected to mongodb atlas

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
+import React, { useState , useEffect} from "react";
 
-function App() {
+//const API_URL = http://localhost:1234/product
+
+
+
+const  App = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    productData();
+  }, []);
+
+  const productData = async () =>{
+    const response = await fetch('http://localhost:1234/product');
+    const data = await response.json();
+    console.log(data)
+    setProducts(data);
+  }
+
+  
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>{products}</h1>
     </div>
   );
 }
