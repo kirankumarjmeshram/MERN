5062618c88f73edbb80ce2725bba870cce834a02
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-04-24 11:29:05 +0530

    added delete functionality for user

--- CRUD/src/controllers/user.controller.js
+++ CRUD/src/controllers/user.controller.js
@@ -35,19 +35,29 @@ router.get("/hello",async (req,res)=>{``
     }
 })
 
-function hello (){
-    return "Hello World";
-}
 
 router.patch("/:id",async (req,res)=>{
     try{
         const user = await User.findByIdAndUpdate(req.params.id,req.body,{new:true}).lean().exec();
+        return res.status(201).send(user)
         
     }catch(err){
         return res.status(500).json({message: err.message,status:"Failed"});
     }
 
 })
+
+router.delete("/:id", async (req,res)=>{
+
+    try{
+        const user=await User.findByIdAndDelete(req.params.id).lean().exec();
+    return res.status(201).send(user)
+    }catch(err){
+        res.status(500).json({message:err.message,status:"Failed"})
+    }
+    
+});
+
 
 
 
