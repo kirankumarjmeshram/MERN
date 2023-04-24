216205b45507ae79433b12eaea82b7f53ad112cb
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-04-24 18:21:37 +0530

    patch successs

--- employeemanagement/client/src/App.js
+++ employeemanagement/client/src/App.js
@@ -1,28 +1,26 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
 //import showProduct './components/showProduct'
-const API_URL = "http://localhost:1234/product";
+const API_URL = "http://localhost:1234/employees";
 
 const App = () => {
   //POST
-  const [products, setProducts] = useState([]);
-  const [name, setName] = useState("");
-  const [image, setImage] = useState("");
-  const [price, setPrice] = useState("");
-  const [ram, setRam] = useState("");
-  const [battery, setBattery] = useState("");
-  const [memory, setMemory] = useState("");
+  const [employees, setEmployees] = useState([]);
+  const [first_name, setFirst_name] = useState("");
+  const [last_name, setLast_name] = useState("");
+  const [email, setEmail] = useState("");
+  const [role, setRole] = useState("");
+  const [employeeId,setEmployeeId]=useState(null)
+
 
   const handleOnSubmit = async (e) => {
     let result = await fetch(API_URL, {
       method: "post",
       body: JSON.stringify({
-        name,
-        img1: image,
-        price1: price,
-        ram,
-        battery,
-        memory,
+        first_name,
+        last_name,
+        email,
+        role,
       }),
       headers: {
         "Content-Type": "application/json",
@@ -33,33 +31,62 @@ const App = () => {
     console.warn(result);
     if (result) {
       alert("Data saved succesfully");
-      setName("");
-      setImage("");
-      setPrice("");
-      setRam("");
-      setBattery("");
-      setMemory("");
+      setFirst_name("");
+      setLast_name("");
+      setEmail("");
+      setRole("");
     }
   };
 
   useEffect(() => {
-    productData();
+    employeeData();
   }, []);
 
-  const productData = async () => {
+  const employeeData = async () => {
     const response = await fetch(API_URL);
     const data = await response.json();
     console.log(data);
-    setProducts(data);
+    setEmployees(data);
   };
   //DELETE
-  function deleteProduct(id){
+  function deleteEmployee(id){
     fetch(`${API_URL}/${id}`,{
       method:'DELETE'
     }).then((result)=>{
       result.json().then((response)=>{
         console.log(response)
-        productData();
+        employeeData();
+      })
+    })
+  }
+
+  //UPDATE
+  function selectEmployee(item)
+  {
+   
+   
+    setFirst_name(item?.first_name)
+    setLast_name(item?.last_name)
+    setEmail(item?.email)
+    setRole(item?.role);
+    setEmployeeId(item?._id)
+      
+  }
+  function updateEmployee()
+  {
+    let item={first_name, last_name,email,role}
+    console.warn("item",item)
+    fetch(`${API_URL}/${employeeId}`, {
+      method: 'PATCH',
+      headers:{
+        'Accept':'application/json',
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify(item)
+    }).then((result) => {
+      result.json().then((resp) => {
+        console.warn(resp)
+        employeeData();
       })
     })
   }
@@ -68,21 +95,33 @@ const App = () => {
     <div className="App">
       {/* POST */}
       <div>
-        <h1>Add Product Data2</h1>
+        <h1>Add Employee Data</h1>
         <form action="">
           <input
             type="text"
-            placeholder="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            placeholder="first name"
+            value={first_name}
+            onChange={(e) => setFirst_name(e.target.value)}
           />
           <input
             type="text"
-            placeholder="image"
-            value={image}
-            onChange={(e) => setImage(e.target.value)}
+            placeholder="last name"
+            value={last_name}
+            onChange={(e) => setLast_name(e.target.value)}
           />
           <input
+            type="text"
+            placeholder="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="role"
+            value={role}
+            onChange={(e) => setRole(e.target.value)}
+          />
+          {/* <input
             type="number"
             pattern="[0-9]*"
             placeholder="price"
@@ -109,25 +148,32 @@ const App = () => {
             placeholder="memory"
             value={memory}
             onChange={(e) => setMemory(e.target.value)}
-          />
+          /> */}
 
           <button type="submit" onClick={handleOnSubmit}>
             submit
           </button>
         </form>
       </div>
+      {/* UPDATE */}
+      <div >
+        <input type="text" value={first_name} onChange={(e)=>{setFirst_name(e.target.value)}} /> <br /><br />
+        <input type="text" value={last_name} onChange={(e)=>{setLast_name(e.target.value)}} /> <br /><br />
+        <input type="text" value={email} onChange={(e)=>{setEmail(e.target.value)}} /> <br /><br />
+        <input type="text" value={role}  onChange={(e)=>{setRole(e.target.value)}} /> <br /><br />
+        <button onClick={updateEmployee} >Update User</button>
+      </div>
       {/* GET */}
       <div>
-        {products.map((elm, i) => {
+        {employees.map((elm, i) => {
           return (
             <div key={i}>
-              <h1>{elm?.name}</h1>
-              <img src={elm?.img1} alt="img1" />
-              <h2>price {elm?.price1}</h2>
-              <h2> ram {elm?.ram}</h2>
-              <h2> battery {elm?.battery}</h2>
-              <h2> memory {elm?.memory}</h2>
-              <button onClick={(e)=>{deleteProduct(elm?._id)}}>Delete Product</button>
+              <h1>{elm?.first_name}</h1>
+              <h1>{elm?.last_name}</h1>
+              <h2>email {elm?.email}</h2>
+              <h2> role {elm?.role}</h2>
+              <button onClick={(e)=>{deleteEmployee(elm?._id)}}>Delete Product</button>
+              <button onClick={(e) => {selectEmployee(elm)}}>Update</button>
               {/* {console.log(elm)} */}
             </div>
           );
