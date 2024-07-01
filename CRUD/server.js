b7998b73dafcc34df7da7795017d08106e5d66c7
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2024-07-01 21:54:18 +0530

    Update server.js

--- CRUD/server.js
+++ CRUD/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const connect = require("./src/configs/db")
+const connect = require("./src/configs/db");
 const userController = require("./src/controllers/user.controller");
 const productController = require("./src/controllers/product.controller");
 const employeeController = require("./src/controllers/employee.controller");
@@ -12,21 +12,16 @@ const app = express();
 
 app.use(express.json());
 
-app.use(bodyParser.urlencoded(
-    { extended:true }
-))
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use("/users", userController);
+app.use("/product", productController);
+app.use("/employees", employeeController);
+app.use("/image", imageController);
+app.use("/img", imgController);
+app.use("/img2", img2Controller);
 
-
-app.use("/users",userController)
-app.use("/product",productController)
-app.use("/employees", employeeController)
-app.use("/image", imageController)
-app.use("/img",imgController)
-app.use("/img2",img2Controller)
-
-
-app.listen(1234,async(req,res)=>{
-    await connect();
-    console.log("Listening on port 1234")
-})
+app.listen(1234, async (req, res) => {
+  await connect();
+  console.log("Listening on port 1234");
+});
